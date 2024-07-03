5bbc3d8d73d66bc0d3e074a37797e1f9b155931d
Abraham May <AbrahamMay1015@gmail.com>
2024-07-03 09:54:59 -0500

    Made it where when the page is refreshed a new feature card will appear and also fixed a problem where the playbutton wouldn't work

--- action.js
+++ action.js
@@ -18,19 +18,60 @@ document.addEventListener('DOMContentLoaded', function() {
     const featureImage = document.getElementById('feature-image');
     const playButton = document.querySelector('.play-button');
     const videoIframe = document.getElementById('video-iframe');
+    const background = document.getElementById('background');
     let selectedBox = null;
 
-    // Function to handle yellow box clicks
-    function handleYellowBoxClick() {
-        yellowBoxes.forEach(box => box.classList.remove('selected'));
-        this.classList.add('selected');
-        selectedBox = this;
-        const featureImageSrc = this.getAttribute('data-feature');
-        featureImage.src = featureImageSrc;
+    const featureImages = [
+        { src: 'Feature/jjkcard.png', video: 'https://www.youtube.com/embed/pkKu9hLT-t8', title: 'Jujutsu Kaisen' },
+        { src: 'Feature/mhacard.png', video: 'https://www.youtube.com/embed/zw5QhH3-1lI', title: 'My Hero Academia' },
+        { src: 'Feature/akamecard.png', video: 'https://www.youtube.com/embed/NIeKMKwON0U', title: 'Akame Ga Kill' },
+        { src: 'Feature/aotcard.png', video: 'https://www.youtube.com/embed/n4Nj6Y_SNYI', title: 'Attack on Titan' },
+        { src: 'Feature/demonslayercard.png', video: 'https://www.youtube.com/embed/Sl2k7bfBeCw', title: 'Demon Slayer' },
+        { src: 'Feature/fatecard.png', video: 'https://www.youtube.com/embed/nfzKXkL_i54', title: 'Fate Stay Night' },
+        { src: 'Feature/fireforcecard.png', video: 'https://www.youtube.com/embed/fzM43HZ6oeg', title: 'Fire Force' },
+        { src: 'Feature/godofhighschoolcard.png', video: 'https://www.youtube.com/embed/oqjwUfprNAk', title: 'God of High School' },
+        { src: 'Feature/opmcard.png', video: 'https://www.youtube.com/embed/YUH1mfV3IEU', title: 'One Punch Man' },
+        { src: 'Feature/vinlandcard.png', video: 'https://www.youtube.com/embed/f8JrZ7Q_p-8', title: 'Vinland Saga' }
+        // Add other images and their corresponding videos and titles here
+    ];
 
-        // Hide the iframe when a new anime is selected
-        videoIframe.style.display = 'none';
-        videoIframe.src = '';
+    function getRandomIndex(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function changeFeature() {
+        const index = getRandomIndex(featureImages.length);
+        const selectedImage = featureImages[index];
+
+        featureImage.src = selectedImage.src;
+        videoIframe.src = selectedImage.video;
+        document.querySelector('.text1 h1').textContent = selectedImage.title;
+
+        // Change background image dynamically
+        background.style.backgroundImage = `url(${selectedImage.src})`;
+
+        // Set selectedBox based on the currently displayed feature image
+        selectedBox = [...yellowBoxes].find(box => box.getAttribute('data-feature') === selectedImage.src);
+    }
+
+    // Initialize on page load
+    changeFeature();
+
+    // Function to handle click on yellow boxes
+    function handleYellowBoxClick() {
+        const backgroundUrl = this.getAttribute('data-background');
+        const featureUrl = this.getAttribute('data-feature');
+        const actionUrl = this.getAttribute('data-action');
+        const videoUrl = this.getAttribute('data-video');
+        const title = this.getAttribute('data-title');
+
+        background.style.backgroundImage = `url(${backgroundUrl})`;
+        featureImage.src = featureUrl;
+        videoIframe.src = videoUrl;
+        document.querySelector('.text1 h1').textContent = title;
+
+        // Set the selectedBox to the current box clicked
+        selectedBox = this;
     }
 
     yellowBoxes.forEach(box => {
@@ -44,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const videoUrl = selectedBox.getAttribute('data-video');
             if (videoUrl) {
                 videoIframe.src = videoUrl;
-                videoIframe.style.width = '1664.47px';
+                videoIframe.style.width = '1666.47px';
                 videoIframe.style.height = '801px';
                 videoIframe.style.display = 'block';
             } else {
@@ -53,7 +94,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Add click event listener to hide the iframe when clicked outside
     document.body.addEventListener('click', function(event) {
         if (!videoIframe.contains(event.target)) {
             videoIframe.style.display = 'none';
